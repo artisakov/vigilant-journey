53461b626bc3bcf1576ff792e16854516b220888
artisakov <60698561+artisakov@users.noreply.github.com>
2020-03-30 19:35:22 +0300

    я делаю тест

--- app/manage.py
+++ app/manage.py
@@ -1,4 +1,4 @@
-from flask import Flask, render_template, request, redirect, url_for, session
+from flask import Flask, render_template, request, redirect, url_for, session 1
 import sqlite3
 import os
 from flask_bootstrap import Bootstrap
